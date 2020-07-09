919c83cd69cb65736034f94cca0e11b5040f8381
xeonus1 <clemens.buchegger@gmx.at>
2020-07-09 12:03:07 +0200

    1. Änderung

--- app/src/main/java/com/example/sensorapp/MainActivity.java
+++ app/src/main/java/com/example/sensorapp/MainActivity.java
@@ -1,25 +1,55 @@
 package com.example.sensorapp;
 
+// Import generiert sich meist selbst (ALT + Enter) ... trotzdem checkern:
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.sensorapp.database.ApplicationDatabase;
 import com.example.sensorapp.database.dao.Datapoint;
 
+
 import java.util.List;
 
+//MAINACTIVITY - FÜR ALLE:
+
+//1 Sensordatenauslesung Stephan Jarisch
+//2 Live-Monitoring Clemens Buchenegger
+//3 Datenbank Felix Edel
+
+//----------------------------------------------------------------------------------------------
 
 public class MainActivity extends AppCompatActivity {
 
-    private ApplicationDatabase database;
+    // Konstruktoren hier anlegen:
+
+    private ApplicationDatabase database; //3
+
+    private SensorManager sensorManager; //1
+
+    private Sensor acceleration; //1
+
+    private LineChart mChart; //2 ????
+
+//-----------------------------------------------------------------------------------------------
 
     @Override
+
+    // Screen on im Emulator -> Beginn von onCreate. Funktionen hier starten.
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 2. Erstellung des Diagramms
+        mChart = (LineChart) findViewById(R.id.chart1);
+
+
 
         database = Room.databaseBuilder(this, ApplicationDatabase.class, "app_db_2")
                 .allowMainThreadQueries()
@@ -41,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
         textView.setText(pointList.size());
 
-        x = 5;
+        int x = 5;
 
     }
 
